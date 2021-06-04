b2d660807288348cf66470401bb3bfc8e9f23cbe
Harish_kumar_hk <40381981+harish355@users.noreply.github.com>
2021-06-04 16:03:22 +0530

    Update settings.py

--- Backend/Api/settings.py
+++ Backend/Api/settings.py
@@ -95,21 +95,21 @@ WSGI_APPLICATION = 'Api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES= {'default': 
-    {'ENGINE': 'djongo', 
-    'NAME': 'fraudify_api', 
-    'CLIENT': {
-        'host': ''
-        }
-    }
-}
+#DATABASES= {'default': 
+ #   {'ENGINE': 'djongo', 
+  #  'NAME': 'fraudify_api', 
+   # 'CLIENT': {
+    #    'host': ''
+     #   }
+   # }
+#}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+}
 
 
 # Password validation
